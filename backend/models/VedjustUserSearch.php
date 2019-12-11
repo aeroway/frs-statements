a90cb6311103de50288704e8f4c365d2a9379fed
frs <frs@webubuntu181.23upr.ru>
2019-12-11 11:37:13 +0300

    11.12

--- backend/models/VedjustUserSearch.php
+++ backend/models/VedjustUserSearch.php
@@ -47,6 +47,7 @@ class VedjustUserSearch extends User
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder' => ['id' => SORT_DESC]],
         ]);
 
         $this->load($params);
